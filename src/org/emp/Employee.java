2912e87d3da09bab354a96a6470013864fbe296e
sshyam1995kumar <sshyam1995kumar@gmail.com>
2020-05-05 18:50:04 +0530

    Tester1 updated the code

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -5,35 +5,10 @@ public class Employee {
 		System.out.println("Employee Id is: 1234");
 	}
 
-	public void empName() {
+	public void empName1() {
 		System.out.println("Employee name is: Shyam Kumar");
 	}
-
-	public void empDob() {
-		System.out.println("Employee DOB is : 5/7/1995");
-	}
-
-	public void empPhone() {
-		System.out.println("Employee Phone no is: 9087238836");
-	}
-
-	private void empEmail() {
-		// TODO Auto-generated method stub
-		System.out.println("Employee Email is: sshyam@gamil.com");
-	}
-
-	public void empAddress() {
-		System.out.println("Address is: Medavakkam,chennai");
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Employee Details:");
-		Employee e = new Employee();
-		e.empId();
-		e.empName();
-		e.empDob();
-		e.empPhone();
-		e.empEmail();
-		e.empAddress();
+	public void empName2() {
+		System.out.println("Employee name is: Shyam");
 	}
 }
